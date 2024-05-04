e09c18f7225ca21bb702c3b87052d642e76dc787
artlermontov2 <artist.bryansk@gmail.com>
2024-05-04 15:35:33 +0300

    update func get weather

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
+import math
 import os
+from datetime import datetime
 import requests
 from dotenv import load_dotenv
 
@@ -8,7 +10,7 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 def get_weather(city, api_key):
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&lang=ru&units=metric'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city.lower()}&appid={api_key}&lang=ru&units=metric'
     responce = requests.get(url)
     
     data = responce.json()
@@ -17,8 +19,12 @@ def get_weather(city, api_key):
     feels = data['main']['feels_like']
     wind = data['wind']['speed']
     city = data['name']
+    sunrise = datetime.fromtimestamp(data["sys"]["sunrise"])
+    sunset = datetime.fromtimestamp(data["sys"]["sunset"])
+    pressure = data['main']['pressure']
+    humidity = data['main']['humidity']
 
-    code_to_smile = {
+    map_to_icon = {
      "Clear": "Ясно \U00002600",
      "Clouds": "Облачно \U00002601",
      "Rain": "Дождь \U00002614",
@@ -28,16 +34,20 @@ def get_weather(city, api_key):
      "Mist": "Туман \U0001F32B"
     }
 
-    if data['weather'][0]['main'] in code_to_smile: 
-        ws = code_to_smile[data['weather'][0]['main']]
+    if data['weather'][0]['main'] in map_to_icon: 
+        ws = map_to_icon[data['weather'][0]['main']]
     else:
         ws = ''
 
     text = (
     f"{city} сейчас:\n"
-    f"Температура: {round(temp)} {ws}\n"
+    f"Температура {round(temp)} {ws}\n"
     f"Ощущается как {round(feels)}\n"
-    f"Ветер: {round(wind)} м\c\n"
+    f"Ветер {round(wind)} м\c\n"
+    f"Давление {math.ceil(pressure/1.333)}\n"
+    f"Влажность {humidity}\n"
+    f"Восход {sunrise.strftime('%H:%M')}\n"
+    f"Закат {sunset.strftime('%H:%M')}"
     )
 
     return text
