8ed35a18a50f98866c76936e4633e5204f8410de
artlermontov2 <artist.bryansk@gmail.com>
2024-05-04 13:48:48 +0300

    update func get weather

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = os.getenv("API_KEY")
+API_KEY = os.getenv("API_KEY")
 
 def get_weather(city, api_key):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&lang=ru&units=metric'
@@ -13,23 +13,34 @@ def get_weather(city, api_key):
     
     data = responce.json()
 
-    heaven = data['weather'][0]['description']
     temp = data['main']['temp']
     feels = data['main']['feels_like']
     wind = data['wind']['speed']
+    city = data['name']
+
+    code_to_smile = {
+     "Clear": "Ясно \U00002600",
+     "Clouds": "Облачно \U00002601",
+     "Rain": "Дождь \U00002614",
+     "Drizzle": "Дождь \U00002614",
+     "Thunderstorm": "Гроза \U000026A1",
+     "Snow": "Снег \U0001F328",
+     "Mist": "Туман \U0001F32B"
+    }
+
+    if data['weather'][0]['main'] in code_to_smile: 
+        ws = code_to_smile[data['weather'][0]['main']]
+    else:
+        ws = ''
 
     text = (
-    f"{city.capitalize()} сейчас:\n"
-    f"Температура: {round(temp)}\n"
+    f"{city} сейчас:\n"
+    f"Температура: {round(temp)} {ws}\n"
     f"Ощущается как {round(feels)}\n"
     f"Ветер: {round(wind)} м\c\n"
-    f"На небе: {heaven}"
     )
 
     return text
 
 
-
-
-
-print(get_weather("брянск", api_key))
+print(get_weather("брянск", API_KEY))
